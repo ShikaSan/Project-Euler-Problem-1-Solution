3a02fdd781121581b5c9d0388f58f96c14cd0452
ShikaSan <jay.lorilla27@gmail.com>
2020-07-02 00:34:15 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int sumOf()
 {
     int ctr;
-    int sum = 0;
+    int sum;
 
     for(ctr = 1; ctr < 1000; ctr++)
     {
@@ -20,7 +20,8 @@ int sumOf()
 
 int main()
 {
-    int num = sumOf();
+    int num = 0;
+    num = sumOf();
 
     cout<< "The sum of the mutiples of 3 and 5 and are natural numbers below 1000 is: " << num << endl;
 
